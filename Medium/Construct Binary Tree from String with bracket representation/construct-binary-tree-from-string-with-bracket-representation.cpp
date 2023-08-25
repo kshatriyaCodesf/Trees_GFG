e17d383ef9c5c19dcf311a44c65a6e033ece6a42
kshatriyaCodesf <134126613+kshatriyaCodesf@users.noreply.github.com>
2023-08-25 23:08:43 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Construct Binary Tree from String with bracket representation/construct-binary-tree-from-string-with-bracket-representation.cpp
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left
+child and a pointer to right child */
+class Node
+{
+public:
+    int data;
+    Node *left, *right;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = this->right = NULL;
+    }
+};
+
+/* This function is to print the inorder of the tree  */
+void inorder(Node *root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+
+
+// } Driver Code Ends
+
+class Solution{
+public:
+    map<int,int>mp;
+    void mapping(string str)
+    {
+        stack<int>st;
+        for(int i = 0 ; i < str.length();i++)
+        {
+            if(str[i]=='(')
+            {
+                st.push(i);
+            }
+            else if(str[i]==')')
+            {
+                mp[st.top()]=i;
+                st.pop();
+            }
+        }
+    }
+    bool isDigit(char ch)
+    {
+        return (ch<='9' and ch>='0');
+    }
+    Node*rec(string str, int si , int ei)
+    {
+        if(si>ei)return NULL;
+        int num = 0;
+        while(si<str.length() and isDigit(str[si]))
+        {
+            int x = str[si]-'0';
+            num = num*10+x;
+            si++;
+        }
+        si--;
+        Node*root = new Node(num);
+        int y = -1;
+        if(si+1<=ei and str[si+1]=='(')
+        {
+            y = mp[si+1];
+        }
+        
+            if(y!=-1)
+            {
+                root->left = rec(str , si+2 , y-1);
+                root->right = rec(str , y+2 , ei-1);
+            }
+            return root;
+
+    }
+
+    Node *treeFromString(string str)
+    {
+        mapping(str);
+        return rec(str , 0 , str.length()-1);
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string str;
+        cin >> str;
+        Solution obj;
+        Node *root = obj.treeFromString(str);
+        inorder(root);
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
